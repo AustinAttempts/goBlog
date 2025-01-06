945b0969177ea0835dcd44cc238626c0de1a967b
--unset <--unset>
2025-01-05 19:39:28 -0800

    renamed, added functionlity to list all blog posts and began refactoring

--- post.go
+++ post.go
@@ -1,13 +1,15 @@
-package main
+package goBlog
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"io"
-	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/adrg/frontmatter"
 	img64 "github.com/tenkoh/goldmark-img64"
@@ -16,24 +18,29 @@ import (
 	"github.com/yuin/goldmark/extension"
 )
 
-func main() {
-	mux := http.NewServeMux()
+type MetadataQuerier interface {
+	Query() ([]PostMetadata, error)
+}
 
-	postTemplate := template.Must(template.ParseFiles("post.gohtml"))
-	fr := FileReader{}
-	mux.HandleFunc("GET /posts/{slug}", PostHandler(fr, postTemplate))
-
-	log.Fatal(http.ListenAndServe(":8080", mux))
+type PostMetadata struct {
+	Slug        string
+	Title       string    `toml:"title"`
+	Author      Author    `toml:"author"`
+	Description string    `toml:"description"`
+	Date        time.Time `toml:"date"`
 }
 
 type SlugReader interface {
 	Read(slug string) (string, error)
 }
 
-type FileReader struct{}
+type FileReader struct {
+	Dir string
+}
 
 func (fr FileReader) Read(slug string) (string, error) {
-	f, err := os.Open(slug + ".md")
+	slugPath := filepath.Join(fr.Dir, slug+".md")
+	f, err := os.Open(slugPath)
 	if err != nil {
 		return "", nil
 	}
@@ -43,6 +50,30 @@ func (fr FileReader) Read(slug string) (string, error) {
 		return "", err
 	}
 	return string(b), nil
+}
+
+func (fr FileReader) Query() ([]PostMetadata, error) {
+	postsPath := filepath.Join(fr.Dir, "*.md")
+	filenames, err := filepath.Glob(postsPath)
+	if err != nil {
+		return nil, fmt.Errorf("querying for files: %w", err)
+	}
+	var posts []PostMetadata
+	for _, filename := range filenames {
+		f, err := os.Open(filename)
+		if err != nil {
+			return nil, fmt.Errorf("querying for files: %w", err)
+		}
+		defer f.Close()
+		var post PostMetadata
+		_, err = frontmatter.Parse(f, &post)
+		if err != nil {
+			return nil, fmt.Errorf("parsing frontmatter for the file %s: %w", filename, err)
+		}
+		post.Slug = strings.TrimSuffix(filepath.Base(filename), ".md")
+		posts = append(posts, post)
+	}
+	return posts, nil
 }
 
 type PostData struct {
@@ -98,6 +129,28 @@ func PostHandler(sl SlugReader, tpl *template.Template) http.HandlerFunc {
 		err = tpl.Execute(w, post)
 		if err != nil {
 			http.Error(w, "Error ecxecuting template", http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+type IndexData struct {
+	Posts []PostMetadata
+}
+
+func IndexHandler(mq MetadataQuerier, tpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		posts, err := mq.Query()
+		if err != nil {
+			http.Error(w, "Error querying posts", http.StatusInternalServerError)
+			return
+		}
+		data := IndexData{
+			Posts: posts,
+		}
+		err = tpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, "Error querying posts", http.StatusInternalServerError)
 			return
 		}
 	}
